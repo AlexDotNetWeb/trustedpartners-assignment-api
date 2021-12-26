d7843f062c4ac9a53676c7ca7e5db20a34c139b1
alex vodz <nasavodz@gmail.com>
2021-12-26 03:59:47 +0200

    Add project files.

---
+++ Controllers/ProxyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+//using System.Text.Json;
+using System.Threading.Tasks;
+using static trustedpartners_assignment.Models.DuckModel;
+
+namespace trustedpartners_assignment.Controllers
+{
+    [EnableCors("ClientPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        IHttpClientFactory _httpClient;
+        public ProxyController(IHttpClientFactory clientFactory)
+        {
+            _httpClient = clientFactory;
+        }
+        [HttpGet("SearchDuckProxy")]
+        public async Task<List<Topics>> SearchDuckProxy(string query)
+        {
+            string uri = $"http://api.duckduckgo.com/?q={query}&format=json";
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), uri))
+                {
+                    var response = await httpClient.SendAsync(request);
+                    using (HttpContent content = response.Content)
+                    {
+                        var jsonString = await response.Content.ReadAsStringAsync();
+                        return  Newtonsoft.Json.JsonConvert.DeserializeObject<MainRoot>(jsonString).RelatedTopics.Where(x=>!string.IsNullOrEmpty(x.FirstURL)&& !string.IsNullOrEmpty(x.Text)).ToList();
+
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Models/DuckModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trustedpartners_assignment.Models
+{
+    public class DuckModel
+    {
+        public class MainRoot
+        {
+            public List<Topics> RelatedTopics { get; set; }
+        }
+
+        public class Topics
+        {
+            public string FirstURL { get; set; }
+            public string Text { get; set; }
+        }
+
+
+
+
+
+
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Icon
+        {
+            public string Height { get; set; }
+            public string URL { get; set; }
+            public string Width { get; set; }
+        }
+
+        public class Topic
+        {
+            public string FirstURL { get; set; }
+            public Icon Icon { get; set; }
+            public string Result { get; set; }
+            public string Text { get; set; }
+        }
+
+        public class RelatedTopic
+        {
+            public string FirstURL { get; set; }
+            public Icon Icon { get; set; }
+            public string Result { get; set; }
+            public string Text { get; set; }
+            public string Name { get; set; }
+            public List<Topic> Topics { get; set; }
+        }
+
+        public class Developer
+        {
+            public string name { get; set; }
+            public string type { get; set; }
+            public string url { get; set; }
+        }
+
+        public class Maintainer
+        {
+            public string github { get; set; }
+        }
+
+        public class SrcOptions
+        {
+            public string directory { get; set; }
+            public int is_fanon { get; set; }
+            public int is_mediawiki { get; set; }
+            public int is_wikipedia { get; set; }
+            public string language { get; set; }
+            public string min_abstract_length { get; set; }
+            public int skip_abstract { get; set; }
+            public int skip_abstract_paren { get; set; }
+            public string skip_end { get; set; }
+            public int skip_icon { get; set; }
+            public int skip_image_name { get; set; }
+            public string skip_qr { get; set; }
+            public string source_skip { get; set; }
+            public string src_info { get; set; }
+        }
+    }
+}
